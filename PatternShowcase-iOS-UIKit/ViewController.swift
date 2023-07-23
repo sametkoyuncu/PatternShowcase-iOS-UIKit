14e201a5811f314136bec74e486c3a61d331edfa
Samet Koyuncu <samet.koyuncu@consultant.turkcell.com.tr>
2023-07-23 09:54:41 +0300

    Initial Commit

---
+++ PatternShowcase-iOS-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatternShowcase-iOS-UIKit
+//
+//  Created by Samet Koyuncu on 23.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
